be0f58ca1d5a2ded9cb635bf39559b375811568d
manosanjay-g <manosanjay.web@gmail.com>
2023-03-17 23:11:05 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+
+//Router Initialization
+
+//DB Init
+const connectDB = require('./configs/database_config')
+//Middleware Initialization
+
+//Middlewares
+app.use(cors())
+app.use(express.json())
+app.use('/public',express.static('public'))
+
+//Routes
+app.get('/',(req,res)=>{
+    res.send('SOCOD');
+})
+
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGODB_URI)
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server listening on PORT ${process.env.PORT}`);
+        })
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+}
+start()
