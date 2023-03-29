99b8795a0d25a74d0beda1f79d8a96521e4cc8c5
manosanjay-g <manosanjay.web@gmail.com>
2023-03-29 20:00:37 +0530

    created userSchema

---
+++ server/models/user_model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name:{type:String,required:true},
+    gender:{type:String,required:true},
+    bio:{type:String,required:true},
+    email:{type:String,required:true},
+    password:{type:String,required:true},
+    dept_id:{type:Number,required:true},
+    year:{type:Number,required:true},
+    liked:{type:Array,default:[]},
+    unliked:{type:Array,default:[]},
+    matched:{type:Array,default:[]}
+})
+
+const userCollection = mongoose.model('user',userSchema,'users')
+
+module.exports = userCollection
