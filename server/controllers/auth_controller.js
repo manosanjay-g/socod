50d8fbd5d2e45f43f07178ec4e1005b932222a2b
manosanjay-g <manosanjay.web@gmail.com>
2023-04-01 22:06:39 +0530

    added email check

--- server/controllers/auth_controller.js
+++ server/controllers/auth_controller.js
@@ -2,22 +2,27 @@ const userModel = require('../models/user_model')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 
-const registerUser = async(req,res)=>{
-    try{
-        const {name,gender,gender_interest,bio,interests,phone,email,password,dept_id,year} = req.body
-        if(!(name && gender && bio && phone && email && password && dept_id && year && gender_interest)){
+const registerUser = async (req, res) => {
+    try {
+        const { name, gender, gender_interest, bio, interests, phone, email, password, dept_id, year } = req.body
+        if (!(name && gender && bio && phone && email && password && dept_id && year && gender_interest)) {
             return res.status(400).json({
-                error:"All fields are required"
+                error: "All fields are required"
             })
         }
-        const oldUser = await userModel.findOne({email});
-        if(oldUser){
+        if (!(email.includes('rajalakshmi.edu.in'))) {
+            return res.status(400).json({
+                error: "Must have an REC email"
+            })
+        }
+        const oldUser = await userModel.findOne({ email });
+        if (oldUser) {
             return res.status(409).json({
-                error:"User already exists"
+                error: "User already exists"
             })
         }
 
-        encryptedPassword = await bcrypt.hash(password,15);
+        encryptedPassword = await bcrypt.hash(password, 15);
 
         const user = await userModel.create({
             name,
@@ -26,38 +31,38 @@ const registerUser = async(req,res)=>{
             bio,
             interests,
             phone,
-            email:email.toLowerCase(),
-            password:encryptedPassword,
+            email: email.toLowerCase(),
+            password: encryptedPassword,
             dept_id,
             year
         })
         const token = jwt.sign({
-            user_id:user._id,
+            user_id: user._id,
             email
         }, process.env.JWT_KEY)
         user.token = token;
         res.status(201).json(user)
-    }catch(err){
+    } catch (err) {
         console.log(err);
     }
 }
-const loginUser = async(req,res)=>{
-    try{
-        const {email,password} = req.body
-        if(!( email && password)){
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        if (!(email && password)) {
             return res.status(400).json({
-                error:"All fields are required"
+                error: "All fields are required"
             })
         }
-        const user = await userModel.findOne({email});
-        if(user == null){
+        const user = await userModel.findOne({ email });
+        if (user == null) {
             return res.status(409).json({
-                error:"User does not exist"
+                error: "User does not exist"
             })
         }
 
-        decryptedPassword = await bcrypt.compare(password,user.password);
-        if(user && decryptedPassword){
+        decryptedPassword = await bcrypt.compare(password, user.password);
+        if (user && decryptedPassword) {
             const token = jwt.sign({
                 user_id: user._id,
                 email
@@ -68,7 +73,7 @@ const loginUser = async(req,res)=>{
         res.status(400).json({
             error: "Invalid Credentials"
         })
-    }catch(err){
+    } catch (err) {
         console.log(err);
     }
 }
