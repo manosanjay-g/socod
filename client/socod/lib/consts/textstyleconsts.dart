38a477dcf96beba24989ac815b981f24bbf3bf76
manosanjay-g <manosanjay.web@gmail.com>
2023-04-13 13:42:40 +0530

    small tweaks

--- client/socod/lib/consts/textstyleconsts.dart
+++
@@ -1,7 +0,0 @@
-
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-Widget headerAccCreation(){
-  return  Text("YOUR", style: TextStyle(
-      fontWeight: FontWeight.bold,fontSize:30),);
-}
